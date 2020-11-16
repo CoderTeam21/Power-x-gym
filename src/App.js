b223818fd419347d0ea1198fa8d515c7593481f0
farjana001 <nipa.farzana87@gmail.com>
2020-11-16 13:40:54 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './Components/Home/Home/Home';
+import Pricing from './Components/Pages/Pricing/Pricing';
+import AboutUsPage from './Components/Pages/AboutUsPage/AboutUsPage';
+import ContactUs from './Components/Pages/ContactUs/ContactUs';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/pricing">
+          <Pricing />
+        </Route>
+        <Route path="/aboutUs">
+          <AboutUsPage />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
